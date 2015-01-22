63096417c2bfaadfea4f06112ac5a0f5fac238b5
Sean <nase00@gmail.com>
2015-01-22 17:49:48 -0600

    Fix travis.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -17,7 +17,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    session[:user_id] = nil
+    session.clear
     redirect_to root_url, :notice => "Signed out!"
   end
 end
