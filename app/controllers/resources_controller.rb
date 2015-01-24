17ea6188c8a0f5d4684ec2a875d6362b427284b2
Sean <nase00@gmail.com>
2015-01-24 17:10:53 -0600

    NeoV

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -2,7 +2,7 @@ class ResourcesController < ApplicationController
   include ApplicationHelper
 
   def show
-    cypher = "START me=node(41)
+    cypher = "START me=node(157)
               OPTIONAL MATCH me -[r]- related
               RETURN me, r, related"
 
