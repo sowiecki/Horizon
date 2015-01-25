0b3cf63429f04abd52e0394fad958ffbf20e487a
Sean <nase00@gmail.com>
2015-01-24 21:24:18 -0600

    Move controller code to appropriate helper.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,8 +7,30 @@ module ApplicationHelper
     results["data"].map {|row| Hash[*results["columns"].zip(row).flatten] }
   end
 
-  def seed_graph
+  def node
+    cypher = "START me=node(#{params[:id]})
+              OPTIONAL MATCH me -[r]- related
+              RETURN me, r, related"
 
+    connections = neo.execute_query(cypher)["data"]
+
+    me = connections[0][0]["data"]
+
+    relationships = []
+    if connections[0][1]
+      connections.group_by{|group| group[1]["type"]}.each do |key,values|
+        relationships <<  { id: key, name: key, values: values.collect{|n| n[2]["data"].merge({id: node_id(n[2]) }) } }
+      end
+    end
+
+    relationships = [{"name" => "No Relationships","values" => [{"id" => "#{params[:id]}","name" => "No Relationships "}]}] if relationships.empty?
+
+    {
+      details_html: "<h2>#{me["username"]}</h2>\n<p class='summary'>\n#{get_properties(me)}</p>\n",
+      data: {
+        attributes: relationships, name: me["username"], id: params[:id]
+      }
+    }
   end
 
   def node_id(node)
