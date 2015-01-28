11fe627565d4e2ea23ad6b59c30e6f218e5b9ea6
DJDeWitt <djdewitt@gmail.com>
2015-01-27 19:13:39 -0600

    created current user profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,79 @@ class UsersController < ApplicationController
   # GET /users/1
   # GET /users/1.json
   def show
+    # Get the User's friend IDs
     @user_friend_ids = client.friend_ids(current_user.username).to_a
+    @categories_hash = {}
+    # Iterate through each category
+    Category.all.each do |category|
+      @categories_hash[category.name] = {}
+      # Iterate through each issue to build our hashy hash of user-specific expert data
+      category.issues.each do |issue_object|
+        @issue_hash = find_correct_users(@user_friend_ids,issue_object)
+
+        @issue_hash[:known].each do |perspective_id|
+          User.find_by(uid: perspective_id.to_s).name
+        end
+
+        @issue_hash[:unknown].each do |perspective_id|
+          User.find_by(uid: perspective_id.to_s).name
+        end
+
+        @categories_hash[category.name][issue_object.name] = @issue_hash
+      end
+
+    end
+
+    ### If I want the amount of experts I follow in Science compared to my total friends
+    p "Categories Hash"
+    p @categories_hash
+
+    # Your Known Experts By Category
+    @known_experts_hash = {}
+    Category.all.each do |category|
+      @known_experts_array = []
+      @categories_hash[category.name].each do |key, value|
+        @known_experts_array << value[:known]
+      end
+      @known_experts_array = @known_experts_array.flatten.uniq
+
+      @known_experts_hash[category.name] = @known_experts_array
+    end
+
+    p "Known Experts Hash"
+    p @known_experts_hash
+
+    # Your Unknown Experts By Category
+    @unknown_experts_hash = {}
+    Category.all.each do |category|
+      @unknown_experts_array = []
+      @categories_hash[category.name].each do |key, value|
+        @unknown_experts_array << value[:unknown]
+      end
+      @unknown_experts_array = @unknown_experts_array.flatten.uniq
+
+      @unknown_experts_hash[category.name] = @unknown_experts_array
+    end
+
+    p "Known Experts Hash"
+    p @known_experts_hash
+    p "Unknown Experts Hash"
+    p @unknown_experts_hash
+
+    # Find Your Least-Followed Category
+    @longest_name = ""
+    @longest_array = []
+    @unknown_experts_hash.each do |name, id_array|
+      if id_array.length >= @longest_array.length
+        @longest_array = id_array
+        @longest_name = name
+      end
+    end
+
+    p @longest_name
+    p @longest_array
+
+
   end
 
   # GET /users/new
@@ -67,4 +139,36 @@ class UsersController < ApplicationController
     def user_params
       params[:user]
     end
+
+
+
+  def find_correct_users(user_friend_ids, issue_object)
+    #the current user's array of ids of people they follow
+    @user_friend_ids = user_friend_ids
+
+    # the current array of expert ids for this issue
+    perspective_ids = return_perspective_ids(issue_object)
+
+    @unknown_friend_ids = []
+    @known_friend_ids = []
+
+    # people in our database you don't currently follow
+    @unknown_friend_ids = perspective_ids - @user_friend_ids
+
+    # people in our database you do currently follow
+
+    @known_friend_ids = @user_friend_ids & perspective_ids
+
+    sorted_perspectives = {unknown: @unknown_friend_ids, known: @known_friend_ids}
+  end
+
+  def return_perspective_ids(issue_object)
+    perspective_ids = []
+    issue_object.perspectives.each do |perspective|
+      perspective_ids << perspective.uid.to_i
+    end
+    perspective_ids
+  end
+
+
 end
