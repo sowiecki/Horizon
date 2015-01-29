177079c1ddf05165c9ed6dcb75bf92ac18c69e3a
Sean <nase00@gmail.com>
2015-01-28 19:38:47 -0600

    Bleh.

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,11 +8,16 @@ class User
     self.twitter = self.twitter.downcase
   end
 
+  after_create do
+    self.set_friend_ids
+  end
+
   property :name, type: String
   property :username, type: String
   property :twitter, type: String
   property :bio, type: String
   property :avatar, type: String
+  property :friend_ids, type: String
   index :twitter
 
   # property :friend_ids, type: Array
@@ -43,5 +48,15 @@ class User
       user.name = auth["info"]["name"]
       user.bio = auth["info"]["description"]
     end
+  end
+
+  def set_friend_ids
+    client = Twitter::REST::Client.new do |config|
+      config.consumer_key        = ENV["CONSUMER_KEY"]
+      config.consumer_secret     = ENV["CONSUMER_SECRET"]
+      config.access_token        = session[:consumer_token]
+      config.access_token_secret = session[:consumer_secret]
+    end
+    self.friend_ids = client.friend_ids(current_user.username).to_a
   end
 end
